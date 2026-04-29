40b9b2f9e10d48afe07d0b1b4d0d5283fa4797ff
Ganadalam <lunar_halo_@naver.com>
2026-04-29 10:48:16 +0900

    markdown_form

---
+++ components/common/dialog/MarkdownDialog.tsx
@@ -0,0 +1,76 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+// import { Button } from "@/components/ui/button";
+import styles from "./MarkdownDialog.module.scss";
+import { Checkbox } from "@/components/ui/checkbox";
+import LabelCalendar from "../calendar/LabelCalendar";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+
+function MarkdownDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        {/* <Button className="font-normal text-gray-400 hover:text-gray-500">
+          Add Contents
+        </Button> */}
+        <span className="font-normal text-gray-400 hover:text-gray-500">
+          Add Contents
+        </span>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            <div className={styles.dialog__titleBox}>
+              <Checkbox className="w-5 h-5" />
+              <input
+                type="text"
+                placeholder="write a title for your board"
+                className={styles.dialog__titleBox__title}
+              />
+            </div>
+          </DialogTitle>
+
+          <DialogDescription className="sr-only">
+            Add a title and set the duration for your new board.
+          </DialogDescription>
+
+          <div className={styles.dialog__calendarBox}>
+            <LabelCalendar label="From" />
+            <LabelCalendar label="To" />
+          </div>
+          <Separator />
+          <div className={styles.dialog__markdown}></div>
+        </DialogHeader>
+        <DialogFooter>
+          <div className={styles.dialog__buttonBox}>
+            <DialogClose asChild>
+              <Button
+                variant={"ghost"}
+                className="font-normal text-gray-400 hover:bg-gray-50 hover:text-gray-500"
+              >
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button
+              type={"submit"}
+              className="font-normal border-orange-500 bg-orange-400 text-white hover:bg-orange-400 hover:text-white"
+            >
+              Cancel
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default MarkdownDialog;
